cafac120301b215499a7df06e457054e994a2b33
jrfs22 <mr.jrfs22@gmail.com>
2025-07-07 15:52:13 +0700

    update: all pages

--- components/footer.tsx
+++ components/footer.tsx
@@ -72,7 +72,7 @@ export function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center">
-          <p className="text-gray-400">© 2024 HateSafe. All rights reserved. Built with ❤️ for safer communities.</p>
+          <p className="text-gray-400">© 2024 S.I.H.I.T.A.M . All rights reserved. Built with ❤️ for safer communities.</p>
         </div>
       </div>
     </footer>
